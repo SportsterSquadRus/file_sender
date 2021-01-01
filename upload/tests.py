1d67f64a4c7b55af57122e6ae61d4e140b373d9c
suren <elek34ka@gmail.com>
2021-01-01 23:17:33 +0300

    test.py

--- upload/tests.py
+++ upload/tests.py
@@ -11,8 +11,6 @@ class FileUploadTests(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
-
-
     def test_list_view(self):
         url = reverse('list_url')
         self.assertEqual(resolve(url).func.view_class, FilesListView)
@@ -21,8 +19,6 @@ class FileUploadTests(TestCase):
         request = self.factory.get('api/list')
         response = DrfListView.as_view()(request)
         self.assertEqual(response.status_code, 200)
-
-
 
     def test_case(self):
         lst = list(map(str, [randint(0, 255) for i in range(10000)]))
@@ -36,12 +32,9 @@ class FileUploadTests(TestCase):
             f.write(lst_str.encode('utf-8'))
             f.seek(0)
             response = c.post(url, {'file': f})
-            
+
         response_get = c.get(url_list)
         data = response_get.content.decode('utf-8')
-        file_data = json.loads(data)
-        lst_from_server = file_data[0]['numbers']
+        lst_from_server = json.loads(data)[0]['numbers']
         self.assertEqual(response.status_code, 200)
         self.assertEqual(lst, lst_from_server)
-
-
