f88727dd7717e17d44e9ce206d19e7af6eaaab05
suren <elek34ka@gmail.com>
2021-01-03 02:10:17 +0300

    256

--- upload/views.py
+++ upload/views.py
@@ -14,7 +14,7 @@ def upload_file(request):
     for key in files:
         data_obj = files[key]
         numbers = tuple(map(lambda x: Number.objects.create(
-            num=int(x) if str(x).isdecimal() and int(x) > 0 else 1), 
+            num=int(x) if str(x).isdecimal() and  256 > int(x) > 0 else 1), 
             data_obj.read().decode('utf-8').split()))
         new_file = FileClass.objects.create(obj=data_obj)
         new_file.numbers.set(numbers)
